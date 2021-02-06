8320f5f7bdbaea7b4bc5ec519be74e3f8e50b757
Josh Wycuff <Joshua.Wycuff@turner.com>
2021-02-06 16:36:19 -0500

    test: cover command-command

--- src/command/command-command.ts
+++ src/command/command-command.ts
@@ -1,41 +1,56 @@
-import * as path from 'path';
+import path from 'path';
 
+// private version of this interface where everything is required
+// eslint-disable-next-line @typescript-eslint/naming-convention
 interface _ICommandCommand {
     path: string;
     name: string;
 }
 
+// public version of this interface where everything is optional
 interface ICommandCommand {
     path?: string;
     name?: string;
 }
 
+// all the types CommandCommand can accept
 export type CommandCommandLike = string | ICommandCommand | CommandCommand;
 
+/**
+ * A class to handle any string manipulation of commands.
+ */
 export class CommandCommand implements _ICommandCommand {
     path: string;
 
     name: string;
 
+    // eslint-disable-next-line require-jsdoc
     constructor(command: CommandCommandLike) {
         if (typeof command === 'string') {
             const iCommandCommand = this.deconstructString(command);
             this.path = iCommandCommand.path;
             this.name = iCommandCommand.name;
         } else {
-            this.path = (command as _ICommandCommand).path;
+            this.path = command.path || '';
             this.name = (command as _ICommandCommand).name;
         }
     }
 
+    /**
+     * Get the string representation of a command.
+     */
     toString() {
         return this.get();
     }
 
+    /**
+     * Get the string representation of a command.
+     */
     get() {
         return path.join(this.path, this.name);
     }
 
+    // eslint-disable-next-line class-methods-use-this
     private deconstructString(str: string): _ICommandCommand {
         return {
             path: path.dirname(str),
---
+++ tests/command/command-command.test.ts
@@ -0,0 +1,28 @@
+import { CommandCommand } from '../../dist/command/command-command';
+
+describe('command-command', () => {
+    it('should take a string', () => {
+        const cmd = new CommandCommand('asdf');
+        expect(cmd.get()).toEqual('asdf');
+    });
+    it('should take a string with relative path', () => {
+        const cmd = new CommandCommand('./asdf/qwer');
+        expect(cmd.get()).toEqual('asdf/qwer');
+    });
+    it('should take a string with absolute path', () => {
+        const cmd = new CommandCommand('/asdf/qwer');
+        expect(cmd.get()).toEqual('/asdf/qwer');
+    });
+    it('should take an object', () => {
+        const cmd = new CommandCommand({ name: 'asdf', path: '/qwer' });
+        expect(cmd.get()).toEqual('/qwer/asdf');
+    });
+    it('should take an object with name only', () => {
+        const cmd = new CommandCommand({ name: 'asdf' });
+        expect(cmd.get()).toEqual('asdf');
+    });
+    it('should have string representation', () => {
+        const cmd = new CommandCommand('asdf');
+        expect(`${cmd}`).toEqual('asdf');
+    });
+});
